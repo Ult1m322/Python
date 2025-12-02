1c851583dd684e2b7cc8738ef8706263355b94d0
Ult1m322 <kishko.op322@gmail.com>
2025-12-02 02:34:22 +0200

    added cars_brand and cars_info models

--- SR_7/duikt_exam_kishko/duikt_task_kishko/models.py
+++ SR_7/duikt_exam_kishko/duikt_task_kishko/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class cars_brand(models.Model):
+    BRAND_NAME = models.CharField(max_length=100)
+    BRAND_COUNTRY = models.CharField(max_length=100)
+    BRAND_RATING = models.IntegerField()
+
+class cars_info(models.Model):
+    CAR_NAME = models.CharField(max_length=100)
+    CAR_MODEL = models.CharField(max_length=100)
+    CAR_PRICE = models.FloatField()
+    CAR_BRAND = models.ForeignKey(cars_brand, on_delete=models.CASCADE)
